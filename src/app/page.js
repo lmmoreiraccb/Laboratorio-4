054f453440522679f7ad2e78d977849e32fe2491
Luis Mauricio Moreira Veliz <lm.moreira@ccb.edu.gt>
2024-08-20 09:07:51 -0600

    Create navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,51 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <main>
+      <header className={styles.header}>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h1>
+            <span>
+              Hello
+              <span className={styles.yellowText}>.</span>
+            </span>
+            <br />
+            <span>
+              <span>I am</span>
+            </span>
+            <br />
+            <span>Douglass Tomas</span>
+          </h1>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </header>
+      <section className={styles.infoSection}>
+        <img
+          src="/beru.jpg"
+          alt="Imagen"
+          className={styles.image}
+          />
+        <div className={styles.infoContainer}>
+          <span className={styles.title + " " + styles.yellowText}>Douglass</span>
+          <br />
+          <span className={styles.title}>Tomas</span>
+          <div className={styles.list}>
+            <ul>
+              <li>
+                <span className={styles.grayText}>Age: </span> 16
+              </li>
+              <li>
+                <span className={styles.grayText}>Nationality: </span> Peru
+              </li>
+              <li>
+                <span className={styles.grayText}>Skill set: </span> Sell IceCreams
+              </li>
+              <li>
+                <span className={styles.grayText}>Langages: </span> English, Spanish
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
